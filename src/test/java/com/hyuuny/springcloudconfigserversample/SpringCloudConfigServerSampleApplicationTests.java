7e9afc7a0234696aa81ddf70d0f9673aa402f088
hyuuny <hyuuny@huray.net>
2023-03-03 15:00:44 +0900

    Initial commit

---
+++ src/test/java/com/hyuuny/springcloudconfigserversample/SpringCloudConfigServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuuny.springcloudconfigserversample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
